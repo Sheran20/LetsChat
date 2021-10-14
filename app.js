3e1ed5de077c9f1a4120b22c6a87cabcb074434f
Sheran <gt-sherantv@hotmail.com>
2021-10-14 11:06:34 -0400

    add procfile, update ports

--- app.js
+++ app.js
@@ -262,6 +262,10 @@ app.post("/login", passport.authenticate("local", {
 // ================
 
 // SERVER HOST
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
